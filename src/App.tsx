33c17fcd4dcb345915d571d73ad6a6da502ba75a
Maksym Honchar <maksim.honchar@gmail.com>
2021-10-09 20:44:10 +0300

    added module css

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import './style.css'
+import Styles from './styles.module.css'
 
 function App() {
 	return (
 		<>
-			<h2 className="hello">React App</h2>
-			<h3>Tets</h3>
+			<h2 className={Styles.hello}>React App</h2>
+			<h2 className="hello">Test</h2>
 		</>
 	)
 }
